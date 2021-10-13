29a10cbd3173935a22c124442f085e5b1f400000
Zhanna Moskaliuk <octoberrock11@gmail.com>
2021-10-13 12:42:58 +0300

    Initial Commit

---
+++ WatchTesterApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  WatchTesterApp WatchKit Extension
+//
+//  Created by Zhanna Moskaliuk on 13.10.2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ WatchTesterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchTesterApp
+//
+//  Created by Zhanna Moskaliuk on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
